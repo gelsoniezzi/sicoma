86cdf09942ca039b5be866b378c32b7c78646b13
unknown <gimg@live.com>
2020-02-17 17:38:21 -0300

    Configurando ambiente

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'gelsonie',
+    database: 'sicoman',
+    define: {
+        timestamps: true, //created_at, updated_at
+        underscored: true
+    }
+
+}
